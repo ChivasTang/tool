569f8698d67847ddbe8437057b309dd48c88b47d
ChivasTang <tangzh1983@gmail.com>
2019-02-17 22:57:36 +0900

    20190217

--- src/main/webapp/WEB-INF/resources/js/common.js
+++ src/main/webapp/WEB-INF/resources/js/common.js
@@ -1,19 +1,23 @@
 $.ajaxSetup({cache: false});
+
 $(function () {
     commFunc.setFun.pageReady();
 });
 
 var commFunc = (function(){
     var setFun={
+
         getLocale: function () {
             var locale=$("#locale").val();
             setFun.setLocale(locale);
         },
 
         setLocale: function (locale) {
+
             if(setFun.isEmpty(locale)){
                 locale=(navigator.language || navigator.browserLanguage).toLowerCase().substring(0,2);
             }
+
             $.i18n.properties({
                 name:"MessageSource",
                 path: "i18n",
@@ -28,6 +32,7 @@ var commFunc = (function(){
         isEmpty:function (str) {
             return undefined===str || null===str || !str || ""===str;
         },
+
         pageReady: function () {
             setFun.getLocale();
         }
